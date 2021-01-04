691b0237f4cb5dc1726e38a3c83419f1f25c5e2f
Justin Lee <jylee@Justins-MacBook-Pro.local>
2021-01-03 22:21:35 -0800

    Finish upfront styling and demo functionality

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import './main.css';
+import VG3 from './VG3.jpg'
 
 class MainPage extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      redirect: null
+    }
+    this.signup = this.signup.bind(this)
+  }
+
+  signup(){
+    this.setState({ redirect: `/signup` })
+  }
 
   render() {
+    if (this.state.redirect != null) {
+      return <Redirect to={this.state.redirect}/>
+    }
+
     return (
       <div id='main-page'>
-        <h1 id='main-h1'>A Squadmaker Corporation</h1>
-        <footer id='footer'>
-          Copyright &copy; 2020 App Academy
-        </footer>
+        <div id='left'>
+          <img className='left-pic' src={VG3} />
+        </div>
+        <div id='middle'>
+          <h1 id='main-h1'>Squad Finding Made Easy</h1>
+          <p id='main-text'>
+              Intuitive squad system to not only join and create squads but to also
+              message other squad members in the same group. Find the perfect 
+              squad for you and get right to gaming with a team
+          </p>
+          <button id='main-demo' onClick={this.signup}>Sign up now!</button>
+        </div>
       </div>
     );
   }
